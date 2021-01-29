71756f589a2087357b4b05ec11a9b1c624b0a098
Jisu Kim <webmaster@alien.moe>
2021-01-29 18:55:36 +0900

    Pass attr & redirect 구현

--- helper/router/HashRouter.js
+++ helper/router/HashRouter.js
@@ -1,8 +1,9 @@
 export class HashRouter {
   routes = null;
   targetElement = null;
-  matches = [];
   currentChild = null;
+  url = null;
+  oldUrl = null;
 
   constructor(targetElement) {
     this.targetElement = targetElement;
@@ -15,38 +16,56 @@ export class HashRouter {
   setRoutes(routes) {
     this.routes = routes;
     Object.freeze(this.routes);
+
+    this.url = new URL(window.location.href);
     this.updateRoute();
   }
 
   updateRoute() {
     const currentRotue = this.matchedRoute;
-    const m = this.hash.match(currentRotue.path);
-    const tmp = [...m].slice(1);
+    // 매치되는 라우트가 없으면 무시
+    if (!currentRotue) return;
 
-    // match값이 달라지면 rotue가 바뀐 것
-    if (JSON.stringify(this.matches) === JSON.stringify(tmp)) return;
-    this.matches = tmp;
+    // redirect
+    if (currentRotue.redirect) {
+      const goto =
+        typeof currentRotue.redirect === "function"
+          ? currentRotue.redirect()
+          : currentRotue.redirect;
+      window.location = `#${goto}`;
+      return;
+    }
+
+    // path 파싱
+    const matches = this.path.match(currentRotue.path);
+    const path = matches[0];
+    // rotue가 바뀐것인지 체크
+    if (this.oldUrl && this.oldUrl.hash.slice(1) === path) return;
+
+    // match 결과 업데이트
+    if (matches && matches.groups) this.groups = matches.groups;
 
     // 원래 있던 Element를 지우고 새로운 Element를 등록
     const newEl = document.createElement(currentRotue.component);
     if (this.currentChild) this.targetElement.removeChild(this.currentChild);
+
+    // match된 값을  attr으로 넘겨줌
+    for (const g in this.groups) newEl.setAttribute(g, this.groups[g]);
     this.targetElement.appendChild(newEl);
     this.currentChild = newEl;
   }
 
-  get hash() {
-    return window.location.hash.slice(1);
+  get path() {
+    return this.url.hash.slice(1) || "/";
   }
 
   get matchedRoute() {
-    return this.routes.find((route) => RegExp(route.path).test(this.hash));
-  }
-
-  get params() {
-    return this.matches;
+    return this.routes.find((route) => RegExp(route.path).test(this.path));
   }
 
   hashChange(event) {
+    this.url = new URL(event.newURL);
+    this.oldUrl = new URL(event.oldURL);
     this.updateRoute();
   }
 }
--- routes.js
+++ routes.js
@@ -1,4 +1,4 @@
 export const routes = [
-  { path: /^\/$/, redirect: () => `/${new Date().getDay()}` },
-  { path: /^\/w\/(\d+)$/, component: "study-app" },
+  { path: /^\/$/, redirect: () => `/w/${new Date().getDay()}` },
+  { path: /^\/w\/(?<week>\d+)$/, component: "study-app" },
 ];
