893e53d03e4ff00e0da5da3fe6a7845d162cffb5
Jisu Kim <webmaster@alien.moe>
2021-02-03 08:13:35 +0900

    상대경로로 변경

--- repo/index.js
+++ repo/index.js
@@ -1,7 +1,7 @@
 export async function getMetadata(week) {
-  return await fetch(`/data/metadata.json?t=${Date.now()}`);
+  return await fetch(`./data/metadata.json?t=${Date.now()}`);
 }
 
 export async function getPlace(placeId) {
-  return await fetch(`/data/${placeId}.json?t=${Date.now()}`);
+  return await fetch(`./data/${placeId}.json?t=${Date.now()}`);
 }
